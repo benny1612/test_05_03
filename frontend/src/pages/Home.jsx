6877ecdf64af6ad299145d73b2c813d6fdedc7b5
benny1612 <bn161299@gmail.com>
2026-03-05 17:13:53 +0200

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,18 +12,17 @@ export default function Home() {
     movie.Title.toLowerCase().includes(search.toLowerCase())
   );
 
-  if (error) return <div className="p-5 text-red-500 text-center">{error}</div>;
 
   return (
-    <div className="bg-white min-h-screen p-4" dir="ltr">
-      <div className="max-w-4xl mx-auto">
+    <div className="bg-white">
+      <div >
         
         <div className="mb-6 text-center">
           <h1 className="text-2xl font-bold mb-4">Movie Night</h1>
           <input
             type="text"
             placeholder="Search movie"
-            className="w-full border p-2 rounded outline-none focus:border-blue-500"
+            className="w-full border p-2 rounded"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
@@ -34,16 +33,16 @@ export default function Home() {
           {search && <span> for: {search}</span>}
         </div>
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 ">
           {filteredMovies.map((movie) => (
-            <div key={movie.imdbID} className="border p-2 rounded shadow-sm flex flex-col justify-between">
+            <div  className="border p-2 rounded">
               <div>
                 <img 
                   src={movie.Images[0]} 
       
-                  className="w-full h-48 object-cover mb-2 rounded"
+                  className="w-full h-48 "
                 />
-                <h3 className="font-bold text-sm truncate">{movie.Title}</h3>
+                <h3 className="font-bold text-sm ">{movie.Title}</h3>
                 <p className="text-xs text-gray-500">{movie.Year}</p>
                 <p className="text-xs text-gray-400 mb-2">{movie.Genre}</p>
               </div>
